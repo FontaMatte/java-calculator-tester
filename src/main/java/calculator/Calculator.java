a5bd9cfdedac45fb204a9636afed77fd3fd8beb0
FontaMatte <matteo.fontanel@gmail.com>
2023-06-19 15:47:04 +0200

    Class Calculator created

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,33 @@
+package calculator;
+
+public class Calculator {
+    private float num1;
+    private float num2;
+
+    // CONSTRUCTOR
+    public Calculator(float num1, float num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    // PUBLIC METHODS
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+
+}
